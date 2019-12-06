1a2a8b9b632b6375b3126c6f36eac0dabfec89b4
ThePunishKnight <37847182+ThePunishKnight@users.noreply.github.com>
2019-12-06 02:39:56 +0200

    level one stage 2
    city i doneeeeeeeeeeeeeeeee

---
+++ galactus the game/Assets/GeneralScripts/Paralux.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralux : MonoBehaviour {
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+	// Use this for initialization
+	void Start () {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+        if (dist < 40)
+        {
+            transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        }
+        if (dist > 40)
+        {
+            transform.position = new Vector3(startpos, transform.position.y, transform.position.z);
+        }
+           if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+	}
+    void FixedUpdate()
+    {
+        
+    }
+}
