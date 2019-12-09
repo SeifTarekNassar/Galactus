11723e9a73447973a741fb1df23905be8c14eb35
said4132 <35873747+said4132@users.noreply.github.com>
2019-12-09 17:26:23 +0200

    Final LVL 4
    Hello world Entring seif

---
+++ galactus the game/Assets/LVL 4/LVL 4/AudioBoss.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBoss : MonoBehaviour {
+
+    public AudioClip fight;
+    public AudioSource Main;
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioManager.instance.randomizefs(fight);
+            Main.Pause();
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioManager.instance.PauseMusic();
+            Main.Play();
+        }
+    }
+}
---
+++ galactus the game/Assets/LVL 4/LVL 4/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static AudioManager instance = null;
+    public float lowpitchrange = .95f;
+    public float highpitchrange = 1.05f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    public void randomizefs(params AudioClip[] clips)
+    {
+        int randomindex = Random.Range(0, clips.Length);
+        float randompitch = Random.Range(lowpitchrange, highpitchrange);
+        efxSource.pitch = randompitch;
+        efxSource.clip = clips[randomindex];
+        efxSource.Play();
+    }
+    public void PauseMusic()
+    {
+        efxSource.Pause();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
